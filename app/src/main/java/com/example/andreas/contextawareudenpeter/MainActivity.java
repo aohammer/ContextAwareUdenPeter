9d50a5824769c9c2cb2a63c8455c2a3342377769
Andreas Hammer <andreas.o.hammer@gmail.com>
2016-11-25 15:46:46 +0100

    Med et par knapper

--- app/src/main/java/com/example/andreas/contextawareudenpeter/MainActivity.java
+++ app/src/main/java/com/example/andreas/contextawareudenpeter/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.andreas.contextawareudenpeter;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+
     }
 }
