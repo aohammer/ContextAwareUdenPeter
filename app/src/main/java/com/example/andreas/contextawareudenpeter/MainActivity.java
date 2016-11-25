7d1ffa4747856d8b2833740ff250d228c48be0a6
Peter <peter91@cs.au.dk>
2016-11-25 17:13:01 +0100

    windows ect.

--- app/src/main/java/com/example/andreas/contextawareudenpeter/MainActivity.java
+++ app/src/main/java/com/example/andreas/contextawareudenpeter/MainActivity.java
@@ -5,15 +5,24 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
+    private List<Double> samples = new ArrayList<>();
+    private int counter = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         //sensor setup
         senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        //fileManager setup
 
         //button setup
         Button startButton = (Button) findViewById(R.id.buttonStart);
@@ -57,9 +68,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Sensor mySensor = event.sensor;
 
         if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
+
+            //Euclidean norm calculation (x^2+y^2+z^2)^(1/2)
+            double accData = Math.sqrt(Math.pow(event.values[0], 2) + Math.pow(event.values[1], 2) + Math.pow(event.values[2], 2));
+
+            //Add to sample window of 128 values
+            for(int counter=0; counter<=128; counter++){
+                samples.add(accData);
+            }
+            if (counter == 128) {
+                //Calculate min, max & std
+                for (Double sample : samples) {
+                }
+            }
         }
     }
 
